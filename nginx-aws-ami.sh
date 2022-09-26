4acca37ae74178bd1540c1233ad38d54d43bfec5
Calum Bradley <calumbradley@Calums-MBP.home>
2022-09-26 21:22:45 +0100

    more tips

--- nginx-aws-ami.sh
+++ nginx-aws-ami.sh
@@ -6,3 +6,12 @@ sudo vi /etc/nginx/nginx.conf
 
 # Fully restart NGINX web server:
 sudo systemctl restart nginx
+
+# install nginx
+sudo yum install nginx
+
+#remove nginx
+sudo yum install nginx
+
+# start nginx
+sudo systemctl start nginx.service
