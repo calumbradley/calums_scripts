f058f01b9b2a77b3e6c56293baf5be3d77489520
Calum Bradley <cal@gmx.co.uk>
2022-09-27 20:39:33 +0100

    certbot cmd and important notes

---
+++ certbot.sh
@@ -0,0 +1,11 @@
+sudo certbot --nginx -d calumbradley.com -d www.calumbradley.com
+
+# IMPORTANT NOTES:
+#  - Congratulations! Your certificate and chain have been saved at:
+#    /etc/letsencrypt/live/calumbradley.com/fullchain.pem
+#    Your key file has been saved at:
+#    /etc/letsencrypt/live/calumbradley.com/privkey.pem
+#    Your certificate will expire on 2022-12-26. To obtain a new or
+#    tweaked version of this certificate in the future, simply run
+#    certbot again with the "certonly" option. To non-interactively
+#    renew *all* of your certificates, run "certbot renew"
